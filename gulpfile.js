f8b65c00f4a6288efaf735b853b0fea66eaa8656
ShindekiSan <abelchuk.maksim@gmail.com>
2021-05-30 21:11:52 +0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,25 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(sass())
     .pipe(gulp.dest("source/css"))
+    .pipe(browserSync.stream());
 });
+
+gulp.task("watch", function () {
+  browserSync.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", browserSync.reload);
+});
+
